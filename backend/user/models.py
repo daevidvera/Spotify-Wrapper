81f584f7dac2abaf5193d8a28c9314631e54513a
Saketh-Budideti <saketh.budideti@gmail.com>
2024-10-24 14:43:34 -0400

    Update to views

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,25 +3,26 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, id, password=None, **extra_fields):
-        if not id:
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
             raise ValueError('The Email field must be set')
         if not password:
             raise ValueError('The password must be set')
 
-        user = self.model(id=id, **extra_fields)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, id, password=None, **extra_fields):
+    def create_superuser(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(id, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
@@ -31,10 +32,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'id'
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} ({self.id})'
+        return f'{self.first_name}, {self.last_name}, {self.username}, {self.id}'
 
 
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import secrets
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from django.shortcuts import redirect
+import urllib.parse
+import base64
+import requests
 # Create your views here.
+
+## Need a view for creating a user (have functionality to check if user exists)
