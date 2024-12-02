42021b300966748a9273a4147d41fdfeb7acd6ff
Saketh-Budideti <saketh.budideti@gmail.com>
2024-12-01 23:52:47 -0500

    Fixed PR errors

--- backend/backend/utils.py
+++
@@ -1,3 +0,0 @@
-# Helper function used to print error messages
-def print_error(msg):
-    print(f'!\n{msg}\n!')
--- backend/user/tasks.py
+++
@@ -1,6 +0,0 @@
-from django.core.management import call_command
-from celery import shared_task
-
-@shared_task
-def run_delete_inactive_users():
-    call_command('delete_inactive_users')
